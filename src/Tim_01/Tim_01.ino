e1c2954ae5fa2b5c0d99abaf47419a7486819a9f
calum <calummelrose@hotmail.com>
2021-05-06 15:39:06 +0100

    delete rpm calc

--- src/Tim_01/Tim_01.ino
+++ src/Tim_01/Tim_01.ino
@@ -1,52 +1,31 @@
 // digital pin 2 is the hall pin
 int hall_pin = 2;
-// set number of hall trips for RPM reading (higher improves accuracy)
-float hall_thresh = 100.0;
-
+float hall_count;
+ 
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(115200);
   // make the hall pin an input:
   pinMode(hall_pin, INPUT);
+
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   // preallocate values for tach
-  float hall_count = 1.0;
-  int counter;
-  float start = micros();
+
   bool on_state = false;
   // counting number of times the hall sensor is tripped
   // but without double counting during the same trip
-  counter = 0;
-  while(true){
-    counter++;
+
     if (digitalRead(hall_pin)==0){
       if (on_state==false){
         on_state = true;
         hall_count+=1.0;
+        Serial.println(hall_count);
       }
     } else{
       on_state = false;
     }
-    
-    if (hall_count>=hall_thresh){
-      break;
-    }
-    if (counter > 10000) {
-      break;
-      }
-  }
-  
-  // print information about Time and RPM
-  float end_time = micros();
-  float time_passed = ((end_time-start)/1000000.0);
-  Serial.print("Time Passed: ");
-  Serial.print(time_passed);
-  Serial.println("s");
-  float rpm_val = (hall_count/time_passed)*60.0;
-  Serial.print(rpm_val);
-  Serial.println(" RPM");
-  delay(1);        // delay in between reads for stability
+
 }
