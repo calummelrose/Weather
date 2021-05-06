bccabfa0f8161b6999fa1decc02abc4e5834d7bf
calum <calummelrose@hotmail.com>
2021-05-06 16:00:21 +0100

    Handle hall effect via interrupt

--- src/Tim_01/Tim_01.ino
+++ src/Tim_01/Tim_01.ino
@@ -8,26 +8,19 @@ void setup() {
   Serial.begin(115200);
   // make the hall pin an input:
   pinMode(hall_pin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(hall_pin), count, RISING);
 
+}
+
+void count(){
+  hall_count++;
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   // preallocate values for tach
 
-  bool on_state = false;
-  // counting number of times the hall sensor is tripped
-  // but without double counting during the same trip
-    
-    bool new_hall;
-
-    new_hall = digitalRead(hall_pin);
-
-    if (new_hall != last_hall) {
-      last_hall= new_hall;
-      hall_count++;
-      Serial.println(hall_count);
-    }
-   
+  Serial.println(hall_count);
+  delay(500);
 
 }
