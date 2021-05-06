a01a8aaa82c78390863b8d616adf8c8b48d6cb42
calum <calummelrose@hotmail.com>
2021-05-06 16:07:19 +0100

    Add rps readout

--- src/Tim_01/Tim_01.ino
+++ src/Tim_01/Tim_01.ino
@@ -1,8 +1,8 @@
 // digital pin 2 is the hall pin
 int hall_pin = 2;
-float hall_count;
-bool last_hall;
- 
+int hall_count, last_hall, new_hall;
+int rps;
+
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(115200);
@@ -18,9 +18,13 @@ void count(){
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // preallocate values for tach
 
-  Serial.println(hall_count);
-  delay(500);
+  last_hall = new_hall;
+  delay(1000);
+  new_hall = hall_count;
+  rps = new_hall - last_hall;
+  Serial.print("RPS: ");
+  Serial.println(rps);
+  
 
 }
